192219338f807a7563507975be94f5c53d65eb5a
Rafael Braga <ralvesbraga40@gmail.com>
2024-11-12 16:23:43 -0500

    Inicio da aula

---
+++ sgcmapi/src/main/java/br/ufac/sgcmapi/model/RespostaErro.java
@@ -0,0 +1,18 @@
+package br.ufac.sgcmapi.model;
+
+import java.util.List;
+
+public class RespostaErro {
+    
+    private List<String> message;
+
+    public List<String> getMessage() {
+        return message;
+    }
+
+    public void setMessage(List<String> message) {
+        this.message = message;
+    }
+
+    
+}
